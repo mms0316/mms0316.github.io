53424c9612181e9f365c4ab02cc162de29355288
mms <lurkingdeeply@gmail.com>
2022-06-01 22:35:06 -0300

    Material List: Added wood conversions

---
+++ materialconversions.js
@@ -0,0 +1,283 @@
+const wood_conversions = {
+    "oak_slab" : [{
+        "oak_planks" : 3,
+    }, 6],
+    "oak_stairs" : [{
+        "oak_planks" : 6,
+    }, 4],
+    "oak_fence" : [{
+        "oak_log" : 1,
+        "stick" : 2
+    }, 3],
+    "oak_button": [{
+        "oak_planks" : 1,
+    }, 1],
+    "oak_pressure_plate": [{
+        "oak_planks" : 2,
+    }, 1],
+    "oak_door": [{
+        "oak_planks" : 6,
+    }, 3],
+    "oak_trapdoor": [{
+        "oak_planks" : 6,
+    }, 2],
+    "oak_fence_gate": [{
+        "oak_planks" : 2,
+        "stick" : 4
+    }, 1],
+    "oak_boat" : [{
+        "oak_planks" : 5
+    }, 1],
+    "oak_sign" : [{
+        "oak_planks" : 6,
+        "stick" : 1
+    }, 3],
+
+    "oak_planks" : [{
+        "oak_log" : 1
+    }, 4],
+    "stripped_oak_log" : [{
+        "oak_log" : 1
+    }, 1],
+    "stripped_oak_wood" : [{
+        "oak_log" : 4
+    }, 3],
+    "oak_wood" : [{
+        "oak_log" : 4,
+    }, 3],
+
+    "spruce_slab" : [{
+        "spruce_planks" : 3,
+    }, 6],
+    "spruce_stairs" : [{
+        "spruce_planks" : 6,
+    }, 4],
+    "spruce_fence" : [{
+        "spruce_log" : 1,
+        "stick" : 2
+    }, 3],
+    "spruce_button": [{
+        "spruce_planks" : 1,
+    }, 1],
+    "spruce_pressure_plate": [{
+        "spruce_planks" : 2,
+    }, 1],
+    "spruce_door": [{
+        "spruce_planks" : 6,
+    }, 3],
+    "spruce_trapdoor": [{
+        "spruce_planks" : 6,
+    }, 2],
+    "spruce_fence_gate": [{
+        "spruce_planks" : 2,
+        "stick" : 4
+    }, 1],
+    "spruce_boat" : [{
+        "spruce_planks" : 5
+    }, 1],
+    "spruce_sign" : [{
+        "spruce_planks" : 6,
+        "stick" : 1
+    }, 3],
+
+    "spruce_planks" : [{
+        "spruce_log" : 1
+    }, 4],
+    "stripped_spruce_log" : [{
+        "spruce_log" : 1
+    }, 1],
+    "stripped_spruce_wood" : [{
+        "spruce_log" : 4
+    }, 3],
+    "spruce_wood" : [{
+        "spruce_log" : 4,
+    }, 3],
+
+    "birch_slab" : [{
+        "birch_planks" : 3,
+    }, 6],
+    "birch_stairs" : [{
+        "birch_planks" : 6,
+    }, 4],
+    "birch_fence" : [{
+        "birch_log" : 1,
+        "stick" : 2
+    }, 3],
+    "birch_button": [{
+        "birch_planks" : 1,
+    }, 1],
+    "birch_pressure_plate": [{
+        "birch_planks" : 2,
+    }, 1],
+    "birch_door": [{
+        "birch_planks" : 6,
+    }, 3],
+    "birch_trapdoor": [{
+        "birch_planks" : 6,
+    }, 2],
+    "birch_fence_gate": [{
+        "birch_planks" : 2,
+        "stick" : 4
+    }, 1],
+    "birch_boat" : [{
+        "birch_planks" : 5
+    }, 1],
+    "birch_sign" : [{
+        "birch_planks" : 6,
+        "stick" : 1
+    }, 3],
+
+    "birch_planks" : [{
+        "birch_log" : 1
+    }, 4],
+    "stripped_birch_log" : [{
+        "birch_log" : 1
+    }, 1],
+    "stripped_birch_wood" : [{
+        "birch_log" : 4
+    }, 3],
+    "birch_wood" : [{
+        "birch_log" : 4,
+    }, 3],
+
+    "jungle_slab" : [{
+        "jungle_planks" : 3,
+    }, 6],
+    "jungle_stairs" : [{
+        "jungle_planks" : 6,
+    }, 4],
+    "jungle_fence" : [{
+        "jungle_log" : 1,
+        "stick" : 2
+    }, 3],
+    "jungle_button": [{
+        "jungle_planks" : 1,
+    }, 1],
+    "jungle_pressure_plate": [{
+        "jungle_planks" : 2,
+    }, 1],
+    "jungle_door": [{
+        "jungle_planks" : 6,
+    }, 3],
+    "jungle_trapdoor": [{
+        "jungle_planks" : 6,
+    }, 2],
+    "jungle_fence_gate": [{
+        "jungle_planks" : 2,
+        "stick" : 4
+    }, 1],
+    "jungle_boat" : [{
+        "jungle_planks" : 5
+    }, 1],
+    "jungle_sign" : [{
+        "jungle_planks" : 6,
+        "stick" : 1
+    }, 3],
+
+    "jungle_planks" : [{
+        "jungle_log" : 1
+    }, 4],
+    "stripped_jungle_log" : [{
+        "jungle_log" : 1
+    }, 1],
+    "stripped_jungle_wood" : [{
+        "jungle_log" : 4
+    }, 3],
+    "jungle_wood" : [{
+        "jungle_log" : 4,
+    }, 3],
+
+    "acacia_slab" : [{
+        "acacia_planks" : 3,
+    }, 6],
+    "acacia_stairs" : [{
+        "acacia_planks" : 6,
+    }, 4],
+    "acacia_fence" : [{
+        "acacia_log" : 1,
+        "stick" : 2
+    }, 3],
+    "acacia_button": [{
+        "acacia_planks" : 1,
+    }, 1],
+    "acacia_pressure_plate": [{
+        "acacia_planks" : 2,
+    }, 1],
+    "acacia_door": [{
+        "acacia_planks" : 6,
+    }, 3],
+    "acacia_trapdoor": [{
+        "acacia_planks" : 6,
+    }, 2],
+    "acacia_fence_gate": [{
+        "acacia_planks" : 2,
+        "stick" : 4
+    }, 1],
+    "acacia_boat" : [{
+        "acacia_planks" : 5
+    }, 1],
+    "acacia_sign" : [{
+        "acacia_planks" : 6,
+        "stick" : 1
+    }, 3],
+
+    "acacia_planks" : [{
+        "acacia_log" : 1
+    }, 4],
+    "stripped_acacia_log" : [{
+        "acacia_log" : 1
+    }, 1],
+    "stripped_acacia_wood" : [{
+        "acacia_log" : 4
+    }, 3],
+    "acacia_wood" : [{
+        "acacia_log" : 4,
+    }, 3],
+
+    "dark_oak_slab" : [{
+        "dark_oak_planks" : 3,
+    }, 6],
+    "dark_oak_stairs" : [{
+        "dark_oak_planks" : 6,
+    }, 4],
+    "dark_oak_fence" : [{
+        "dark_oak_log" : 1,
+        "stick" : 2
+    }, 3],
+    "dark_oak_button": [{
+        "dark_oak_planks" : 1,
+    }, 1],
+    "dark_oak_pressure_plate": [{
+        "dark_oak_planks" : 2,
+    }, 1],
+    "dark_oak_door": [{
+        "dark_oak_planks" : 6,
+    }, 3],
+    "dark_oak_trapdoor": [{
+        "dark_oak_planks" : 6,
+    }, 2],
+    "dark_oak_fence_gate": [{
+        "dark_oak_planks" : 2,
+        "stick" : 4
+    }, 1],
+    "dark_oak_boat" : [{
+        "dark_oak_planks" : 5
+    }, 1],
+    "dark_oak_sign" : [{
+        "dark_oak_planks" : 6,
+        "stick" : 1
+    }, 3],
+
+    "dark_oak_planks" : [{
+        "dark_oak_log" : 1
+    }, 4],
+    "stripped_dark_oak_log" : [{
+        "dark_oak_log" : 1
+    }, 1],
+    "stripped_dark_oak_wood" : [{
+        "dark_oak_log" : 4
+    }, 3],
+    "dark_oak_wood" : [{
+        "dark_oak_log" : 4,
+    }, 3],
+}
