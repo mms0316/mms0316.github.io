0307065855849ec154e332d0a270146d04424b89
mms <lurkingdeeply@gmail.com>
2022-07-03 23:37:52 -0300

    Schematic Previewer: Initial crude version

---
+++ materialcolors.js
@@ -0,0 +1,61 @@
+const minecraft_color = new Map([
+    ['minecraft:white_wool', 0xdcdcdc],
+    ['minecraft:light_gray_wool', 0x848484],
+    ['minecraft:gray_wool', 0x414141],
+    ['minecraft:black_wool', 0x151515],
+    ['minecraft:brown_wool', 0x58412c],
+    ['minecraft:red_wool', 0x842c2c],
+    ['minecraft:orange_wool', 0xba6d2c],
+    ['minecraft:yellow_wool', 0xc5c52c],
+    ['minecraft:lime_wool', 0x6db015],
+    ['minecraft:green_wool', 0x586d2c],
+    ['minecraft:cyan_wool', 0x416d84],
+    ['minecraft:light_blue_wool', 0x5884ba],
+    ['minecraft:blue_wool', 0x2c4199],
+    ['minecraft:purple_wool', 0x6d3699],
+    ['minecraft:magenta_wool', 0x9941ba],
+    ['minecraft:pink_wool', 0xd06d8e],
+
+    ['minecraft:white_terracotta', 0xD3B4A4],
+    ['minecraft:light_gray_terracotta', 0x896D64],
+    ['minecraft:gray_terracotta', 0x3C2B26],
+    ['minecraft:black_terracotta', 0x271812],
+    ['minecraft:brown_terracotta', 0x503525],
+    ['minecraft:red_terracotta', 0x913F32],
+    ['minecraft:orange_terracotta', 0xA05327],
+    ['minecraft:yellow_terracotta', 0xB88421],
+    ['minecraft:lime_terracotta', 0x687737],
+    ['minecraft:green_terracotta', 0x4D542A],
+    ['minecraft:cyan_terracotta', 0x545959],
+    ['minecraft:light_blue_terracotta', 0x726D8C],
+    ['minecraft:blue_terracotta', 0x4B3D5B],
+    ['minecraft:purple_terracotta', 0x764455],
+    ['minecraft:magenta_terracotta', 0x95586C],
+    ['minecraft:pink_terracotta', 0xA04E4E],
+
+    ['minecraft:mushroom_stem', 0xDAD6CB],
+    ['minecraft:packed_ice', 0xA2C0F5],
+
+    ['minecraft:cobblestone', 0x626262],
+    ['minecraft:cobblestone_slab', 0x626262],
+
+    ['minecraft:slime_block', 0x79C668],
+
+    ['minecraft:sand', 0xFBF4BD],
+
+    ['minecraft:sandstone', 0xC5BD75],
+    ['minecraft:sandstone_slab', 0xC5BD75],
+
+    ['minecraft:diorite', 0x9D9DA0],
+    ['minecraft:diorite_slab', 0x9D9DA0],
+
+    ['minecraft:prismarine_brick', 0x64A08F],
+    ['minecraft:prismarine_brick_slab', 0x64A08F],
+
+    ['minecraft:crimson_hyphae', 0x521810],
+    ['minecraft:warped_hyphae', 0x442131],
+
+    ['minecraft:crimson_slab', 0x5C3042],
+    ['minecraft:warped_slab', 0x398382],
+
+]);
